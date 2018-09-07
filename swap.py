58e8f2f16069a468654600d7b993e960bc27c577
Ivan Cvitkovic <icvitkovic@gmail.com>
2018-09-07 11:58:36 -0700

    Add script for swapping source and translation; for back-translation

---
+++ swap.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+"""
+Swap source (msgid) and translation (msgstr); removing source language
+"""
+import sys
+import polib
+
+po_filename = sys.argv[1]
+po = polib.pofile(po_filename)
+
+translated_only = polib.POFile()
+translated_only.metadata=po.metadata
+
+for entry in po:
+    if entry.msgid == entry.msgstr:
+        continue
+
+    reversed_entry = polib.POEntry(
+        msgid=entry.msgstr,
+        msgstr="",
+        occurrences=entry.occurrences
+    )
+    translated_only.append(reversed_entry)
+
+print translated_only
